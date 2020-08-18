0b9ed04ef01a29b6119b00c91b288a4832e774af
Victor Cesar Lopez <civ187@gmail.com>
2020-08-18 13:54:50 -0500

    08/18/2020 @ 1354

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
+const Post = require('./Post');
 
 
 
@@ -7,4 +8,4 @@ const User = require('./User');
 
 
 
-module.exports = { User };
+module.exports = { User, Post };
