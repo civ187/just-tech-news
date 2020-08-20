645fb11dc600b7a26669c083ab447e29d3cc2080
Victor Cesar Lopez <civ187@gmail.com>
2020-08-19 22:56:14 -0500

    debugging heroku

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-// updated the name of the user-routes.js file from User-routes.js
-const userRoutes = require('./user-routes');
+
+const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
